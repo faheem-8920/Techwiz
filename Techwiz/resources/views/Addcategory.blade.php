a91e3001989f671c2c838e4d167dcac452ae5c8f
MuhammadShamsuddin <shamsuddin2288@gmail.com>
2026-09-24 05:32:02 -0400

    CATEGORY CRUD WORK DONE

---
+++ Techwiz/resources/views/Addcategory.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Category</title>
+</head>
+<body>
+
+    <h2>Add Category</h2>
+
+    <form action="{{ url('/Addcategorylogic') }}" method="POST">
+        @csrf
+
+        <div>
+            <label>Category Name</label>
+            <input type="text" name="Name" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label>Category Type</label>
+
+            <select name="type" required>
+                <option value="">Select Category Type</option>
+                <option value="income">Income</option>
+                <option value="expense">Expense</option>
+            </select>
+        </div>
+
+        <br>
+
+        <button type="submit">Add Category</button>
+
+    </form>
+
+</body>
+</html>
